c91029f26c1bd8eb612463e63a1dccfb9b70f803
c3nsored <u.gabrielr@gmail.com>
2024-07-29 23:58:48 +0200

    layered limiters

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/glerarch/rate_limiter
 
 go 1.22.2
 
-require golang.org/x/time v0.5.0
+require (
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
+	golang.org/x/time v0.5.0
+)
--- rate_limiter.go
+++ rate_limiter.go
@@ -2,30 +2,74 @@ package ratelimiter
 
 import (
 	"context"
+	"sort"
 	"time"
 
 	"golang.org/x/time/rate"
 )
 
+type RateLimiter interface {
+	Wait(context.Context) error
+	Limit() rate.Limit
+}
+
+func NewMultiLimiter(limits ...RateLimiter) *multiLimiter {
+	byLimit := func(i, j int) bool {
+		return limits[i].Limit() < limits[j].Limit()
+	}
+	sort.Slice(limits, byLimit)
+
+	return &multiLimiter{limiters: limits}
+}
+
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func (l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
+
 func Open() *APIConnection {
+
 	return &APIConnection{
-		rateLimiter: rate.NewLimiter(rate.Limit(1), 1),
+		networkLimit: NewMultiLimiter(
+			rate.NewLimiter(Per(3, time.Second), 3),
+		),
+		diskLimit: NewMultiLimiter(
+			rate.NewLimiter(rate.Limit(1), 1),
+		),
+		apiLimit: NewMultiLimiter(
+			rate.NewLimiter(Per(2, time.Second), 1),
+			rate.NewLimiter(Per(10, time.Minute), 10),
+		),
 	}
 }
 
 type APIConnection struct {
-	rateLimiter *rate.Limiter
+	networkLimit,
+	diskLimit,
+	apiLimit RateLimiter
 }
 
 func (a *APIConnection) Readfile(ctx context.Context) error {
-	if err := a.rateLimiter.Wait(ctx); err != nil {
+	if err := NewMultiLimiter(a.apiLimit, a.networkLimit).Wait(ctx); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (a *APIConnection) ResolveAddress(ctx context.Context) error {
-	if err := a.rateLimiter.Wait(ctx); err != nil {
+	if err := NewMultiLimiter(a.apiLimit, a.networkLimit).Wait(ctx); err != nil {
 		return err
 	}
 
